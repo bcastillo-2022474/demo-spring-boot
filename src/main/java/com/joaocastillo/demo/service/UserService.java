d543c62679463637a636d410fbf3830e7645f3cf
jcastillo21 <bcastillo-2022474@kinal.edu.gt>
2023-11-15 20:30:35 -0600

    created different application properties for dev and prod environments

--- src/main/java/com/joaocastillo/demo/service/UserService.java
+++ src/main/java/com/joaocastillo/demo/service/UserService.java
@@ -3,7 +3,9 @@ package com.joaocastillo.demo.service;
 import com.joaocastillo.demo.domain.User;
 import com.joaocastillo.demo.exceptions.BadCredentialsException;
 import com.joaocastillo.demo.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -22,10 +24,10 @@ public class UserService {
     public User login(String email, String password) throws BadCredentialsException {
         User user = userRepository.findByEmail(email);
         if (user == null) {
-            throw new BadCredentialsException("User not found");
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not found");
         }
         if (!user.getPassword().equals(password)) {
-            throw new BadCredentialsException("Invalid password");
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid password");
         }
         return user;
     }
